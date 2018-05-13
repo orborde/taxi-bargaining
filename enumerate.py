0df825f7a332f7a2162c38da2d23ac56cc77e7cf
William Ehlhardt <williamehlhardt@gmail.com>
2018-05-13 00:24:11 -0700

    Start generating new possible worlds

--- enumerate.py
+++ enumerate.py
@@ -53,6 +53,17 @@ class Coalition(namedtuple('Coalition', ['passengers', 'taxi'])):
         new_passengers = {p:0 for p in self.passengers}
         return Coalition(frozendict(new_passengers), None)
 
+    def remove(self, participant):
+        assert participant in Participants
+        if participant in Passengers:
+            return self.remove_passenger(participant)
+        else:
+            return self.remove_taxi(participant)
+
+    def __in__(self, x):
+        assert x is not None
+        return (x in self.passengers) or (taxi == x)
+
     def possible_removals(self):
         removals = []
         for p in self.passengers:
@@ -62,6 +73,41 @@ class Coalition(namedtuple('Coalition', ['passengers', 'taxi'])):
             removals.append( (t, self.remove_taxi(t)) )
 
         return removals
+
+
+# https://docs.python.org/2/library/itertools.html#recipes
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
+
+def assemble_coalition(participants):
+    passengers = [p for p in participants if p in Passengers]
+    taxis = [t for t in participants if t in Taxis]
+    assert len(taxis)+len(passengers) == len(participants)
+
+    if len(taxis) > 1:
+        return None
+
+    if len(taxis) == 1:
+        taxi = taxis[0]
+    else:
+        taxi = None
+
+    if len(passengers) == 0 and taxi is None:
+        return None
+
+    return passengers, taxi
+
+def fares_for_coalition(passengers, taxi):
+    assert (len(passengers) > 0) or (taxi is not None)
+
+    if taxi is None or len(passengers) == 0:
+        yield Coalition(frozendict({p:0 for p in passengers}), taxi)
+        return
+
+    for fares in itertools.product(range(PassengerMaxPrice+1), repeat=len(passengers)):
+        yield Coalition(frozendict(zip(passengers,fares)), taxi)
 
 class World(namedtuple('World', ['coalitions'])):
     def valid(self):
@@ -77,6 +123,38 @@ class World(namedtuple('World', ['coalitions'])):
 
         return True
 
+    def possible_new_worlds(self):
+        prev_coalition = {}
+        def record_in(x, c):
+            assert x not in prev_coalition
+            prev_coalition[x] = c
+
+        for c in self.coalitions:
+            for p in c.passengers:
+                record_in(p, c)
+            if c.taxi is not None:
+                record_in(c.taxi, c)
+
+        for defectors in powerset(Participants):
+            coalition = assemble_coalition(defectors)
+            if coalition is None:
+                continue
+
+            passengers, taxi = coalition
+            revised_old_coalitions = []
+            for c in self.coalitions:
+                for x in defectors:
+                    if x in c:
+                        c = c.remove(x)
+                revised_old_coalitions.append(c)
+
+            for new_coalition in fares_for_coalition(passengers, taxi):
+                new_coalitions = revised_old_coalitions + [new_coalition]
+                new_world = World(frozenset(new_coalitions))
+                assert new_world.valid()
+                yield defectors, new_world
+
+
 print 'Generating possible worlds...'
 
 def partitions(arr, start=0):
@@ -89,32 +167,6 @@ def partitions(arr, start=0):
         for subparts in partitions(arr, start=end):
             yield [slice] + subparts
     return
-
-def fares_for_coalition(passengers, taxi):
-    assert (len(passengers) > 0) or (taxi is not None)
-
-    if taxi is None or len(passengers) == 0:
-        yield Coalition(frozendict({p:0 for p in passengers}), taxi)
-        return
-
-    for fares in itertools.product(range(PassengerMaxPrice+1), repeat=len(passengers)):
-        yield Coalition(frozendict(zip(passengers,fares)), taxi)
-
-def assemble_coalition(participants):
-    passengers = [p for p in participants if p in Passengers]
-    taxis = [t for t in participants if t in Taxis]
-    assert len(taxis)+len(passengers) == len(participants)
-
-    if len(taxis) > 1:
-        return None
-
-    if len(taxis) == 1:
-        taxi = taxis[0]
-    else:
-        taxi = None
-
-    return passengers, taxi
-
 
 def construct_worlds(partition):
     subset_ranges = []
@@ -141,3 +193,8 @@ def gen_all_worlds():
 
 all_worlds = gen_all_worlds()
 print len(all_worlds), 'possible worlds'
+
+print 'Checking...'
+for w in all_worlds:
+    for defectors, new_world in w.possible_new_worlds():
+        assert new_world in all_worlds
